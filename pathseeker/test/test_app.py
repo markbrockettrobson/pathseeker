6e689b8be615c1f0a637d273dddcbf1907c16b51
MarkBrockettRobson <markbrockettrobson@gmail.com>
2020-01-11 15:05:50 +1100

    remove blank line

--- pathseeker/test/test_app.py
+++ pathseeker/test/test_app.py
@@ -4,7 +4,6 @@ import pathseeker.src.app as app
 
 
 class TestAtLeastAPI(unittest.TestCase):
-
     def test_app_created(self):
         application = app.APP.test_client()
         self.assertIsNotNone(application)
