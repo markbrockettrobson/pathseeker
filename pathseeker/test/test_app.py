3216d6210b50b216075692b73d9025d2c0013817
MarkBrockettRobson <markbrockettrobson@gmail.com>
2020-01-11 14:59:30 +1100

    lint files

--- pathseeker/test/test_app.py
+++ pathseeker/test/test_app.py
@@ -1,11 +1,10 @@
 import unittest
 
-import flask
-
 import pathseeker.src.app as app
 
 
 class TestAtLeastAPI(unittest.TestCase):
 
+    @staticmethod
     def test_app_created(self):
         app.APP.test_client()
