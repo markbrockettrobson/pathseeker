b06e9ed5ba3b66813ec2a51e9608e281e07cfabd
Mark Brockett Robson <MarkBrockettRobson@gmail.com>
2020-05-16 20:38:36 +1000

    add test for DB non null on import of app

--- pathseeker/test/test_app.py
+++ pathseeker/test/test_app.py
@@ -3,7 +3,11 @@ import unittest
 import pathseeker.src.app as app
 
 
-class TestAtLeastAPI(unittest.TestCase):
+class TestApp(unittest.TestCase):
     def test_app_created(self):
         application = app.APP.test_client()
         self.assertIsNotNone(application)
+
+    def test_db_created(self):
+        database = app.DB
+        self.assertIsNotNone(database)
