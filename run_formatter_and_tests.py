950bdc8f1a985f98da617fd3fc2e56d3464be9b4
markbrockettrobson <markbrockettrobson@gmail.com>
2021-05-26 00:12:52 +1000

    change to from imports

--- run_formatter_and_tests.py
+++ run_formatter_and_tests.py
@@ -1,21 +1,21 @@
-import os
-import subprocess
-import typing
+from os import environ
+from subprocess import run
+from typing import List, Optional, Dict
 
 
 def main():
-    def run_command(command: typing.List[str], env: typing.Optional[typing.Dict[str, str]] = None):
+    def run_command(command: List[str], env: Optional[Dict[str, str]] = None):
         joined_command = " ".join(command)
-        working_env = os.environ.copy()
+        working_env = environ.copy()
 
         if env is None:
             print(f"> {joined_command}")
-            subprocess.run(command, check=True, env=working_env, shell=True)
+            run(command, check=True, env=working_env, shell=True)
         else:
             for name, value in env.items():
                 working_env[name] = value
             print(f"> {joined_command}  {env}")
-            subprocess.run(command, check=True, env=working_env, shell=True)
+            run(command, check=True, env=working_env, shell=True)
 
     run_command(["echo", "%cd%"])
     run_command(["black", "."])
